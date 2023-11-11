3735bd6efa97995e6de14ed61299c48a9c1ac816
Inhwan Yu <kcryu2003@naver.com>
2023-11-11 14:20:48 +0900

    commit 3

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("hello")
+import requests
+
+url = 'http://ws.bus.go.kr/api/rest/buspos/getBusPosByRouteSt'
+params ={'serviceKey' : '서비스키', 'busRouteId' : '124000009', 'startOrd' : '1', 'endOrd' : '10' }
+
+response = requests.get(url, params=params)
+print(response.content)
