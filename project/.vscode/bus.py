add1b021e88d73106378313c923a4f41ab014700
Inhwan Yu <kcryu2003@naver.com>
2023-11-14 18:48:10 +0900

    commit 6

---
+++ project/.vscode/bus.py
@@ -0,0 +1,9 @@
+
+#import requests
+#url = 'http://ws.bus.go.kr/api/rest/buspos/getBusPosByRouteSt'
+#params ={'serviceKey' : 'OPS4fohR6WPWKsxqowtr%2B4b4tg053PyFaFhn3iIo0pyXfMA1Bsyx4x1j7P0TOmVbA67Y5V%2FUrGoQwyCO83K4LQ%3D%3D', 'busRouteId' : '823', 'startOrd' : '1', 'endOrd' : '10' }
+
+#response = requests.get(url, params=params)
+#print(response.content)
+http://ws.bus.go.kr/api/rest/buspos/getBusPosByRtid?ServiceKey=OPS4fohR6WPWKsxqowtr+4b4tg053PyFaFhn3iIo0pyXfMA1Bsyx4x1j7P0TOmVbA67Y5V/UrGoQwyCO83K4LQ==
+&busRouteId=100100118
