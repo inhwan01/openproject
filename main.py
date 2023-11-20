51eeb79a85ae0977a5e957ea809e0ab1d0731611
Inhwan Yu <kcryu2003@naver.com>
2023-11-20 21:10:59 +0900

    commit 9

--- main.py
+++ main.py
@@ -1,37 +1,46 @@
+from flask import Flask, render_template
 import requests
 import xml.etree.ElementTree as ET
 
-url = 'http://ws.bus.go.kr/api/rest/buspos/getLowBusPosByRtid'  # 저상버스위치조회
-params = {
-    'serviceKey': '서비스키',
-    'busRouteId': '124000038',
-    'resultType': 'xml'
-}
+app = Flask(__name__)
 
-response = requests.get(url, params=params)
+@app.route('/')
+def index():
+    # 백엔드 API 엔드포인트 주소
+    api_url = 'http://ws.bus.go.kr/api/rest/buspos/getLowBusPosByRtid'
+    
+    # API 호출 및 데이터 가져오기
+    response = requests.get(api_url, params={
+        'serviceKey': 'OPS4fohR6WPWKsxqowtr+4b4tg053PyFaFhn3iIo0pyXfMA1Bsyx4x1j7P0TOmVbA67Y5V/UrGoQwyCO83K4LQ==',
+        'busRouteId': '124000038',
+        'resultType': 'xml'
+    })
 
-if response.status_code == 200:
-    try:
-        # XML 형식의 응답을 파싱
-        root = ET.fromstring(response.text)
+    if response.status_code == 200:
+        try:
+            # XML 형식의 응답을 파싱
+            root = ET.fromstring(response.text)
+            
+            # XML 구조를 기반으로 원하는 데이터 추출
+            msg_body = root.find(".//msgBody")
+            if msg_body is not None:
+                item_list = msg_body.findall(".//itemList")
 
-        # XML 구조를 기반으로 원하는 데이터 추출
-        msg_body = root.find(".//msgBody")
-        if msg_body is not None:
-            item_list = msg_body.findall(".//itemList")
-
-            if item_list:
-                for item in item_list:
-                    bus_number = item.findtext('plainNo', '')
-                    bus_location = item.findtext('tmX', ''), item.findtext('tmY', '')
-
-                    print(f"버스 번호: {bus_number}, 위치: {bus_location}")
+                if item_list:
+                    bus_data = [
+                        {'bus_number': item.findtext('plainNo', ''),
+                         'bus_location': (item.findtext('tmX', ''), item.findtext('tmY', ''))}
+                        for item in item_list
+                    ]
+                    return render_template('front.html', bus_data=bus_data)
+                else:
+                    return "데이터가 없습니다."
             else:
-                print("데이터가 없습니다.")
-        else:
-            print("응답 데이터의 구조가 예상과 다릅니다.")
-    except ET.ParseError as e:
-        print(f"XML 파싱 오류: {e}")
-else:
-    print(f"API 요청 실패: {response.status_code}")
-    print(response.text)
+                return "응답 데이터의 구조가 예상과 다릅니다."
+        except ET.ParseError as e:
+            return f"XML 파싱 오류: {e}"
+    else:
+        return f"API 요청 실패: {response.status_code}"
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // 페이지 로딩이 완료되면 실행됨
-
     // 백엔드 API 엔드포인트 주소
-    const apiUrl = 'http://ws.bus.go.kr/api/rest/buspos/getLowBusPosByRtid';
+    const apiUrl = 'http://ws.bus.go.kr/api/rest/buspos/getLowBusPosByRtid?serviceKey=OPS4fohR6WPWKsxqowtr+4b4tg053PyFaFhn3iIo0pyXfMA1Bsyx4x1j7P0TOmVbA67Y5V/UrGoQwyCO83K4LQ==&busRouteId=124000038&resultType=xml';
 
     // 버스 정보를 표시할 div 요소
     const busInfoDiv = document.getElementById('bus-info');
@@ -10,21 +8,41 @@ document.addEventListener('DOMContentLoaded', function () {
     // 백엔드에서 데이터를 받아오는 함수
     async function getBusInfo() {
         try {
+            // 백엔드로 요청 보내기
             const response = await fetch(apiUrl);
-            const data = await response.json();
 
-            // 받아온 데이터를 HTML에 삽입
-            data.forEach(bus => {
-                const busNumber = bus.plainNo;
-                const busLocation = `X: ${bus.tmX}, Y: ${bus.tmY}`;
+            // 응답을 텍스트로 받아오기
+            const responseText = await response.text();
 
-                // 새로운 div 요소 생성
-                const busDiv = document.createElement('div');
-                busDiv.innerHTML = `<p>버스 번호: ${busNumber}</p><p>위치: ${busLocation}</p>`;
+            // XML 파싱
+            const parser = new DOMParser();
+            const xmlDoc = parser.parseFromString(responseText, 'application/xml');
 
-                // busInfoDiv에 새로 생성한 div 추가
-                busInfoDiv.appendChild(busDiv);
-            });
+            // XML 구조를 기반으로 원하는 데이터 추출
+            const msgBody = xmlDoc.querySelector('msgBody');
+            if (msgBody !== null) {
+                const itemNodes = msgBody.querySelectorAll('itemList');
+
+                if (itemNodes.length > 0) {
+                    itemNodes.forEach(item => {
+                        const busNumber = item.querySelector('plainNo').textContent;
+                        const tmX = item.querySelector('tmX').textContent;
+                        const tmY = item.querySelector('tmY').textContent;
+                        const busLocation = `X: ${tmX}, Y: ${tmY}`;
+
+                        // 새로운 div 요소 생성
+                        const busDiv = document.createElement('div');
+                        busDiv.innerHTML = `<p>버스 번호: ${busNumber}</p><p>위치: ${busLocation}</p>`;
+
+                        // busInfoDiv에 새로 생성한 div 추가
+                        busInfoDiv.appendChild(busDiv);
+                    });
+                } else {
+                    console.log("데이터가 없습니다.");
+                }
+            } else {
+                console.log("응답 데이터의 구조가 예상과 다릅니다.");
+            }
         } catch (error) {
             console.error('Error fetching bus information:', error);
         }
